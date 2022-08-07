88d489f7434a3273180611373ea5ed89484336e2
Jose Antonio <jams19rubi@gmail.com>
2022-08-07 20:27:11 +0200

    Initial commit - Blog created

---
+++ pages/[slug].js
@@ -0,0 +1,27 @@
+import {MDXRemote} from 'next-mdx-remote'
+import { getFileBySlug, getFiles } from "../lib/mdx"
+import MDXComponents from '../components/MDXComponents';
+
+export default function Post({source, frontmatter})  {
+    return <MDXRemote {...source} components={MDXComponents}/>
+}
+
+export async function getStaticProps({params}) {
+    const {source, fronmatter} = await getFileBySlug(params.slug)
+    return {
+        props: { source, fronmatter}
+    }
+}
+
+export async function getStaticPaths(){
+    const posts = await getFiles()
+    const paths = posts.map((post) => ({
+        params: {
+            slug: post.replace(/\.mdx/, "")
+        }
+    }))
+    return {
+        paths,
+        fallback: false,
+    }
+}
